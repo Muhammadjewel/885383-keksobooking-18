96443e4bf6f77ce6bd3e65210b2c19ad21a93a2d
muhammadjewel <msuratov@gmail.com>
2019-09-22 18:29:58 +0500

    Generate mock description, price, rooms and guests count

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@
 
 var MOCK_DATA = {
   'count': 8,
-  'randomText': 'Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Заголовок меня залетают, безорфографичный взобравшись рот большой но по всей сбить правилами жаренные вершину домах осталось единственное образ диких всеми гор своего за продолжил своих рыбными свой подпоясал скатился пунктуация.'
+  'randomText': 'Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Собрал великий составитель запятой переулка имеет осталось lorem коварных, рыбными свою? Страна залетают ее то несколько путь lorem точках он меня если приставка ipsum напоивший, страну над жизни выйти коварный домах оксмокс вскоре коварных, запятых. Вскоре lorem повстречался но буквенных обеспечивает, деревни своего эта напоивший вершину ему она продолжил возвращайся вопроса, переулка имени за буквоград, последний правилами. Подзаголовок, предупредила, ты! Речью если власти вопроса домах грустный запятых букв! Выйти, вскоре, дороге! Даль гор, первую взгляд себя скатился повстречался строчка всеми мир. Буквенных свой дал, но меня единственное. Ручеек своих lorem деревни великий вершину использовало лучше, грустный заголовок инициал семантика домах напоивший все необходимыми рукопись продолжил но семь продолжил грамматики меня? Коварный, это дорогу которое однажды. Осталось путь своих силуэт текста выйти заглавных дороге, толку предупредила пор. Точках наш, строчка вопрос.',
+  'maxWordsInTitle': 5,
+  'price': {
+    'min': 3000,
+    'max': 15000
+  },
+  'rooms': {
+    'min': 1,
+    'max': 8
+  },
+  'maxGuests': 12,
+  'maxWordsInDescription': 35
 };
 
 var MAP_CANVAS_WIDTH = 600;
@@ -34,8 +45,12 @@ var generateData = function () {
     var locationY = getRandomIntInclusive(MAP_CANVAS_TOP_Y, MAP_CANVAS_BOTTOM_Y);
     var newDataObject = {
       'offer': {
-        'title': getRandomText(MOCK_DATA.randomText, 4),
-        'address': locationX + ', ' + locationY
+        'title': getRandomText(MOCK_DATA.randomText, MOCK_DATA.maxWordsInTitle),
+        'address': locationX + ', ' + locationY,
+        'price': getRandomIntInclusive(MOCK_DATA.price.min, MOCK_DATA.price.max),
+        'rooms': getRandomIntInclusive(MOCK_DATA.rooms.min, MOCK_DATA.rooms.max),
+        'guests': getRandomIntInclusive(0, MOCK_DATA.maxGuests),
+        'description': getRandomText(MOCK_DATA.randomText, MOCK_DATA.maxWordsInDescription)
       },
       'location': {
         'x': locationX,
