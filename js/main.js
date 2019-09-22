bca114a891fd1d97930a457371df4ab59a27acce
muhammadjewel <msuratov@gmail.com>
2019-09-22 18:20:05 +0500

    Refactor mock data into an object

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
-var DATA_LENGTH = 8;
-var RANDOM_TEXT = 'Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Заголовок меня залетают, безорфографичный взобравшись рот большой но по всей сбить правилами жаренные вершину домах осталось единственное образ диких всеми гор своего за продолжил своих рыбными свой подпоясал скатился пунктуация.';
+var MOCK_DATA = {
+  'count': 8,
+  'randomText': 'Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Заголовок меня залетают, безорфографичный взобравшись рот большой но по всей сбить правилами жаренные вершину домах осталось единственное образ диких всеми гор своего за продолжил своих рыбными свой подпоясал скатился пунктуация.'
+};
+
 var MAP_CANVAS_WIDTH = 600;
 var MAP_CANVAS_TOP_Y = 130;
 var MAP_CANVAS_BOTTOM_Y = 630;
@@ -26,12 +29,12 @@ function getRandomIntInclusive(min, max) {
 
 var generateData = function () {
   var dataArray = [];
-  for (var i = 0; i < DATA_LENGTH; i++) {
+  for (var i = 0; i < MOCK_DATA.count; i++) {
     var locationX = getRandomIntInclusive(0, MAP_CANVAS_WIDTH);
     var locationY = getRandomIntInclusive(MAP_CANVAS_TOP_Y, MAP_CANVAS_BOTTOM_Y);
     var newDataObject = {
       'offer': {
-        'title': getRandomText(RANDOM_TEXT, 4),
+        'title': getRandomText(MOCK_DATA.randomText, 4),
         'address': locationX + ', ' + locationY
       },
       'location': {
