6945d0cd6d8ea50001ea0ae45fa611b744b481fe
muhammadjewel <msuratov@gmail.com>
2019-09-22 17:08:37 +0500

    Generate offer location

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 var DATA_LENGTH = 8;
 var RANDOM_TEXT = 'Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Заголовок меня залетают, безорфографичный взобравшись рот большой но по всей сбить правилами жаренные вершину домах осталось единственное образ диких всеми гор своего за продолжил своих рыбными свой подпоясал скатился пунктуация.';
+var MAP_CANVAS_WIDTH = 600;
+var MAP_CANVAS_TOP_Y = 130;
+var MAP_CANVAS_BOTTOM_Y = 630;
 
 var getRandomText = function (text, wordCount) {
   var splittedText = text.split(' ');
@@ -9,14 +12,34 @@ var getRandomText = function (text, wordCount) {
   return splittedText.splice(startingPoint, wordCount).join(' ');
 };
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var generateData = function () {
   var dataArray = [];
   for (var i = 0; i < DATA_LENGTH; i++) {
-    dataArray.push({
+    var locationX = getRandomIntInclusive(0, MAP_CANVAS_WIDTH);
+    var locationY = getRandomIntInclusive(MAP_CANVAS_TOP_Y, MAP_CANVAS_BOTTOM_Y);
+    var newDataObject = {
       'offer': {
-        'title': getRandomText(RANDOM_TEXT, 4)
+        'title': getRandomText(RANDOM_TEXT, 4),
+        'address': locationX + ', ' + locationY
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
       }
-    });
+    };
+    dataArray.push(newDataObject);
   }
   return dataArray;
 };
