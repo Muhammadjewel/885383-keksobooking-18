fc99e0f42191c81dfcdf78e0cd3651d2ee394a6b
muhammadjewel <msuratov@gmail.com>
2019-10-14 11:27:56 +0500

    Refactor random number generation and mock title generation functions

--- js/main.js
+++ js/main.js
@@ -37,47 +37,44 @@ var getRandomText = function (textsArray, arrayIndex) {
   return textsArray[arrayIndex];
 };
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+function getRandomNumber(min, max) {
+  var minValue = Math.ceil(min);
+  var maxValue = Math.floor(max);
 
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (maxValue - minValue + 1));
 }
 
 var getAvatarPath = function (suffixNumber) {
   return 'img/avatars/user0' + suffixNumber + '.png';
 };
 
+var generateMockTitle = function (index) {
+  getRandomText(MOCK_DATA.announcementTitles, index);
+};
+
 var generateData = function () {
   var dataArray = [];
 
   for (var i = 0; i < MOCK_DATA.count; i++) {
-    var locationX = getRandomIntInclusive(0, MAP_CANVAS_WIDTH - PIN_WIDTH / 2);
-    var locationY = getRandomIntInclusive(MAP_CANVAS_TOP_Y + PIN_HEIGHT, MAP_CANVAS_BOTTOM_Y);
+    var locationX = getRandomNumber(0, MAP_CANVAS_WIDTH - PIN_WIDTH / 2);
+    var locationY = getRandomNumber(MAP_CANVAS_TOP_Y + PIN_HEIGHT, MAP_CANVAS_BOTTOM_Y);
 
     var newDataObject = {
       'author': {
         'avatar': getAvatarPath(i + 1)
       },
       'offer': {
-        'title': getRandomText(MOCK_DATA.announcementTitles, i),
+        'title': generateMockTitle(i),
         'address': locationX + ', ' + locationY,
-        'price': getRandomIntInclusive(MOCK_DATA.price.min, MOCK_DATA.price.max),
-        'type': MOCK_DATA.type[getRandomInt(0, MOCK_DATA.type.length)],
-        'rooms': getRandomIntInclusive(MOCK_DATA.rooms.min, MOCK_DATA.rooms.max),
-        'guests': getRandomIntInclusive(0, MOCK_DATA.maxGuests),
-        'checkin': MOCK_DATA.checkin[getRandomInt(0, MOCK_DATA.checkin.length)],
-        'checkout': MOCK_DATA.checkout[getRandomInt(0, MOCK_DATA.checkout.length)],
-        'features': MOCK_DATA.features.slice(0, getRandomInt(1, MOCK_DATA.features.length)),
+        'price': getRandomNumber(MOCK_DATA.price.min, MOCK_DATA.price.max),
+        'type': MOCK_DATA.type[getRandomNumber(0, MOCK_DATA.type.length)],
+        'rooms': getRandomNumber(MOCK_DATA.rooms.min, MOCK_DATA.rooms.max),
+        'guests': getRandomNumber(0, MOCK_DATA.maxGuests),
+        'checkin': MOCK_DATA.checkin[getRandomNumber(0, MOCK_DATA.checkin.length)],
+        'checkout': MOCK_DATA.checkout[getRandomNumber(0, MOCK_DATA.checkout.length)],
+        'features': MOCK_DATA.features.slice(0, getRandomNumber(1, MOCK_DATA.features.length)),
         'description': getRandomText(MOCK_DATA.descriptionTexts, i),
-        'photos': MOCK_DATA.photos.slice(0, getRandomInt(1, MOCK_DATA.photos.length))
+        'photos': MOCK_DATA.photos.slice(0, getRandomNumber(1, MOCK_DATA.photos.length))
       },
       'location': {
         'x': locationX,
